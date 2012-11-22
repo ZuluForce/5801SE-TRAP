30cd78b67df49a1dbc2246c165a2099b8d18614a
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-11-21 20:57:42 -0600

    Adding empty classes for rules.

---
+++ src/edu/umn/se/trap/rules/input/GrantPercentSumTo100.java
@@ -0,0 +1,11 @@
+// GrantPercentSumTo100.java
+package edu.umn.se.trap.rules.input;
+
+/**
+ * @author Dylan
+ *
+ */
+public class GrantPercentSumTo100
+{
+
+}
---
+++ src/edu/umn/se/trap/rules/input/TransportMilesTraveledIsInt.java
@@ -0,0 +1,11 @@
+// TransportMilesTraveledIsInt.java
+package edu.umn.se.trap.rules.input;
+
+/**
+ * @author Dylan
+ *
+ */
+public class TransportMilesTraveledIsInt
+{
+
+}
