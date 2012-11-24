a99ead32f8871ccef128ed53c803cada305ce590
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-11-24 03:26:31 -0600

    Have all of my rules roughly finished. Also added US carriers enum. Found the list of US based air carriers on the moodle forum.

---
+++ src/edu/umn/se/trap/data/TransportationCarrierEnum.java
@@ -0,0 +1,42 @@
+// TransportationCarrierEnum.java
+package edu.umn.se.trap.data;
+
+/**
+ * An enumeration of all possible US air carriers.
+ * 
+ * @author Dylan
+ * 
+ */
+public enum TransportationCarrierEnum
+{
+    /** Southwest Airlines */
+    Southwest,
+
+    /** Alaska Airlines */
+    Alaska_Airlines,
+
+    /** American Airlines */
+    American,
+
+    /** Delta Airlines */
+    Delta,
+
+    /** Frontier Airlines */
+    Frontier,
+
+    /** Great Lakes Airlines */
+    Great_Lakes,
+
+    /** Spirit Airlines */
+    Spirit,
+
+    /** Sun Country Airlines */
+    Sun_Country,
+
+    /** United Airlines */
+    United,
+
+    /** US Airways */
+    US_Airways
+
+}
--- src/edu/umn/se/trap/rules/input/USCarriersOnly.java
+++ src/edu/umn/se/trap/rules/input/USCarriersOnly.java
@@ -1,11 +1,53 @@
 // USCarriersOnly.java
 package edu.umn.se.trap.rules.input;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.umn.se.trap.data.ReimbursementApp;
+import edu.umn.se.trap.data.TransportationCarrierEnum;
+import edu.umn.se.trap.data.TransportationExpense;
+import edu.umn.se.trap.exception.FormProcessorException;
+import edu.umn.se.trap.exception.InputValidationException;
+
 /**
  * @author Dylan
- *
+ * 
  */
-public class USCarriersOnly
+public class USCarriersOnly extends InputValidationRule
 {
+    private static Logger log = LoggerFactory.getLogger(USCarriersOnly.class);
 
+    @Override
+    public void checkRule(ReimbursementApp app) throws InputValidationException,
+            FormProcessorException
+    {
+        List<TransportationExpense> transportationExpenses = app.getTransportationExpenseList();
+
+        if (transportationExpenses.isEmpty())
+        {
+            throw new InputValidationException("Missing transportation expenses");
+        }
+
+        for (TransportationExpense transportationExpense : transportationExpenses)
+        {
+            if (transportationExpense.getTransportationType().toString() == "AIR")
+            {
+                // TODO Not sure if this is going to work. Can switch to a for loop if it does not.
+                try
+                {
+                    TransportationCarrierEnum.valueOf(transportationExpense
+                            .getTransportationCarrier());
+                }
+                catch (Exception e)
+                {
+                    throw new InputValidationException(
+                            "Air carrier is not recognized or not a US based carrier.");
+                }
+            }
+        }
+
+    }
 }
