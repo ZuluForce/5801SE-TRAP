45d8377b0a1f434815be26ecdee8b429041c55b3
Brian T Maurer <maure113@umn.edu>
2012-11-24 12:53:08 -0600

    Adding Class Outlines

---
+++ src/edu/umn/se/trap/rules/business/CarRental.java
@@ -0,0 +1,24 @@
+package edu.umn.se.trap.rules.business;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.umn.se.trap.data.Grant;
+import edu.umn.se.trap.data.OtherExpense;
+import edu.umn.se.trap.data.ReimbursementApp;
+import edu.umn.se.trap.db.GrantDBWrapper;
+import edu.umn.se.trap.db.KeyNotFoundException;
+import edu.umn.se.trap.exception.BusinessLogicException;
+import edu.umn.se.trap.exception.TRAPException;
+
+/**
+ *
+ * @author maure113
+ *
+ */
+public class CarRental extends BusinessLogicRule
+{
+    return;
+}
