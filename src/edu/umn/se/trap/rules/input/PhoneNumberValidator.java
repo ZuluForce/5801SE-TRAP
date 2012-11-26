deee71ac0333b349dcf71da036ae8f762782c06a
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-11-25 21:44:56 -0600

    Added comments and cleaned up the code

--- src/edu/umn/se/trap/rules/input/PhoneNumberValidator.java
+++ src/edu/umn/se/trap/rules/input/PhoneNumberValidator.java
@@ -1,17 +1,15 @@
 /*****************************************************************************************
  * Copyright (c) 2012 Dylan Bettermann, Andrew Helgeson, Brian Maurer, Ethan Waytas
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  ****************************************************************************************/
 // PhoneNumberValidator.java
 package edu.umn.se.trap.rules.input;
@@ -19,22 +17,24 @@ package edu.umn.se.trap.rules.input;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import edu.umn.se.trap.data.ReimbursementApp;
 import edu.umn.se.trap.exception.FormProcessorException;
 import edu.umn.se.trap.exception.InputValidationException;
 
 /**
+ * Check that phone numbers are formatted in a specific format (for consistency). This format is
+ * ddd-ddd-dddd where d is a positive integer
+ * 
+ * 
  * @author Dylan
  * 
  */
 public class PhoneNumberValidator extends InputValidationRule
 {
-    /** Logger for the PhoneNumberValidator class */
-    private static Logger log = LoggerFactory.getLogger(PhoneNumberValidator.class);
-
+    /**
+     * Check that phone numbers are formatted in a specific format (for consistency). This format is
+     * ddd-ddd-dddd where d is a positive integer
+     */
     @Override
     public void checkRule(ReimbursementApp app) throws InputValidationException,
             FormProcessorException
@@ -49,15 +49,22 @@ public class PhoneNumberValidator extends InputValidationRule
 
         isValid = isValidPhoneNumber(phoneNumber);
 
-        // TODO Write a better exception message.
         if (isValid == false)
         {
-            throw new InputValidationException(String.format(
-                    "The phone number (%s) is not formatted correctly.", phoneNumber));
+            throw new InputValidationException(
+                    String.format(
+                            "The phone number (%s) is not formatted correctly. The correct format is 123-456-7890.",
+                            phoneNumber));
         }
 
     }
 
+    /**
+     * Test the submitted phone number against a regular expression to check for valid format.
+     * 
+     * @param phoneNumber - phone number associated with a trap user
+     * @return - a boolean representing if the phone number format is valid or not
+     */
     public boolean isValidPhoneNumber(String phoneNumber)
     {
         String expression = "\\d{3}-\\d{3}-\\d{4}";
