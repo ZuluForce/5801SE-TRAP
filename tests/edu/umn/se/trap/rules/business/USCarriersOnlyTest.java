f8b8a9d946d9da8ba3b1700c5daeb1ef50a68d95
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-12-06 15:56:17 -0600

    Pushing small changes so I can pull.

--- tests/edu/umn/se/trap/rules/business/USCarriersOnlyTest.java
+++ tests/edu/umn/se/trap/rules/business/USCarriersOnlyTest.java
@@ -1,11 +1,34 @@
 // USCarriersOnlyTest.java
 package edu.umn.se.trap.rules.business;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import edu.umn.se.test.frame.TrapTestFramework;
+import edu.umn.se.trap.exception.TRAPException;
+
 /**
  * @author Dylan
- *
+ * 
  */
-public class USCarriersOnlyTest
+public class USCarriersOnlyTest extends TrapTestFramework
 {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setup() throws TRAPException
+    {
+
+    }
+
+    @Test
+    public void validUSCarrier() throws TRAPException
+    {
+
+    }
 
 }
