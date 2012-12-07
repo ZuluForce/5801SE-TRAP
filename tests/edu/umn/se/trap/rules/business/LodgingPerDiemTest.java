e4ad58ce8e98850b80b02e8ddaca270211773bed
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-12-06 21:21:27 -0600

    working on lodging per diem

--- tests/edu/umn/se/trap/rules/business/LodgingPerDiemTest.java
+++ tests/edu/umn/se/trap/rules/business/LodgingPerDiemTest.java
@@ -1,12 +1,17 @@
 // LodgingPerDiemTest.java
 package edu.umn.se.trap.rules.business;
 
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import edu.umn.se.test.frame.TestPerDiemDB;
 import edu.umn.se.test.frame.TrapTestFramework;
 import edu.umn.se.trap.exception.TRAPException;
+import edu.umn.se.trap.form.InputFieldKeys;
 import edu.umn.se.trap.test.generate.LoadedSampleForm;
+import edu.umn.se.trap.test.generate.TestDataGenerator.SampleDataEnum;
 
 /**
  * @author Dylan
@@ -17,12 +22,35 @@ public class LodgingPerDiemTest extends TrapTestFramework
     Integer formId;
     LoadedSampleForm formData;
 
+    String lodging1Name, lodging1Amount;
+    String lodging2Name, lodging2Amount;
+
+    TestPerDiemDB.PerDiemBuilder builder;
+
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
+    @Before
     public void setup() throws TRAPException
     {
         setValidUser();
+        formData = getLoadableForm(SampleDataEnum.DOMESTIC1);
+        formId = this.saveFormData(formData, "test form");
+
+        // lodging1Name = String.format(InputFieldKeys.LODGING_CURRENCY_FMT, 1);
+        lodging1Amount = String.format(InputFieldKeys.LODGING_AMOUNT_FMT, 1);
+
+        lodging2Amount = String.format(InputFieldKeys.LODGING_AMOUNT_FMT, 2);
     }
 
+    // One day - under per diem amount
+    @Test
+    public void singleLodgingExpenseLessThanPerDiemLimit() throws TRAPException
+    {
+        // formData.put(lodging1Amount, value)
+    }
+
+    // Two days - under per diem amount
+    // One day - over per diem amount
+    // Two days - over per diem amount
 }
