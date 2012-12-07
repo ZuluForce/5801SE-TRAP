53b34c5e9fb15c24ca3aaa69b298ada0aee84b87
Dylan Bettermann <dylan.bettermann@gmail.com>
2012-12-06 20:10:52 -0600

    US

--- tests/edu/umn/se/trap/rules/business/USCarriersOnlyTest.java
+++ tests/edu/umn/se/trap/rules/business/USCarriersOnlyTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import edu.umn.se.test.frame.TrapTestFramework;
+import edu.umn.se.trap.exception.BusinessLogicException;
 import edu.umn.se.trap.exception.TRAPException;
 import edu.umn.se.trap.form.InputFieldKeys;
 import edu.umn.se.trap.test.generate.LoadedSampleForm;
@@ -21,7 +22,7 @@ public class USCarriersOnlyTest extends TrapTestFramework
     LoadedSampleForm formData;
     Integer formId;
 
-    String airCarrierField;
+    String airCarrier;
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
@@ -31,22 +32,28 @@ public class USCarriersOnlyTest extends TrapTestFramework
     {
         setValidUser();
         formData = getLoadableForm(SampleDataEnum.DOMESTIC1);
-        formId = this.saveFormData(formData, "a test form");
+        formId = this.saveFormData(formData, "test form");
 
-        airCarrierField = String.format(InputFieldKeys.TRANSPORTATION_CARRIER_FMT, 1);
+        airCarrier = String.format(InputFieldKeys.TRANSPORTATION_CARRIER_FMT, 1);
 
     }
 
     @Test
     public void validUSCarrier() throws TRAPException
     {
-
+        formData.put(airCarrier, "American");
+        this.saveFormData(formData, formId);
+        submitFormData(formId);
     }
 
     @Test
     public void invalidCarrier() throws TRAPException
     {
-
+        exception.expect(BusinessLogicException.class);
+        exception.expectMessage("is not a valid US carrier");
+        formData.put(airCarrier, "Dylan's Party Plane");
+        this.saveFormData(formData, formId);
+        submitFormData(formId);
     }
 
 }
